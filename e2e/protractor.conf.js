511b38c218e263a2e42f5f762d735508041beb4f
Andre Kohn <andre@kohn.io>
2015-08-11 21:54:04 +0200

    Using npm install and clear dependencies

--- e2e/protractor.conf.js
+++ e2e/protractor.conf.js
@@ -1,4 +1,13 @@
 exports.config = {
+  // use jasmine spec reporter instead of the dots
+  onPrepare: function() {
+      var SpecReporter = require('jasmine-spec-reporter');
+      jasmine.getEnv().addReporter(new SpecReporter({displayStacktrace: true}));
+   }
+
+  // remove the dots completely
+  print: function() {}
+
   allScriptsTimeout: 11000,
 
   specs: [
