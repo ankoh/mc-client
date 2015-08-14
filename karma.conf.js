5357a61400b7dd19fd45c611c45d8b128534fe89
Andre Kohn <andre@kohn.io>
2015-08-15 01:20:53 +0200

    The problem was angular-material 0.10.1. Rolling back to 0.10.0 resolves the issues

--- karma.conf.js
+++ karma.conf.js
@@ -12,9 +12,6 @@ module.exports = function(config){
 
     files : [
       'app/bower_components/angular/angular.js',
-      'app/bower_components/angular-route/angular-route.js',
-      'app/bower_components/angular-aria/angular-aria.js',
-      'app/bower_components/angular-mocks/angular-mocks.js',
       'app/bower_components/angular-material/angular-material.js',
       'app/components/**/*.js',
       'app/view*/**/*.js'
