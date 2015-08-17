83dfc45f7ab3d0ba0974286c746b704588ecfdd8
Andre Kohn <andre@kohn.io>
2015-08-17 23:38:44 +0200

    Smaller layout changes on system page

--- app/src/controllers/system.js
+++ app/src/controllers/system.js
@@ -4,7 +4,7 @@ function SystemController() {
 
 	// linechart
 	this.linechart.labels = ["11.08", "12.08", "13.08", "14.08", "15.08", "16.08", "17.08"];
-	this.linechart.series = ['API Calls', 'File Downloads', 'BibTex Downloads'];
+	this.linechart.series = ['API Calls', 'File Downloads', 'BibTeX Downloads'];
 	this.linechart.data = [
 		[65, 59, 80, 81, 56, 55, 40],
 		[28, 48, 40, 19, 33, 27, 30],
@@ -14,17 +14,6 @@ function SystemController() {
 	this.linechart.options = {
 		maintainAspectRatio: false,
 		responsive: true
-	};
-	
-
-	// piechart
-	this.piechart.labels = ["Download Sales", "In-Store Sales", "Mail-Order Sales"];
-  	this.piechart.data = [300, 500, 100];
-  	this.piechart.colours = ['#002143', '#AEAEAE', '#4E4E4E'];
-  	this.piechart.options = {
-		maintainAspectRatio: false,
-		responsive: true,
-		animation: false
 	};
 	
 }
