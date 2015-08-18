e45897481912f999c7c61b0888394075ecee12d8
Andre Kohn <andre@kohn.io>
2015-08-18 12:51:47 +0200

    Work on system and statistics page

--- app/src/controllers/statistics.js
+++ app/src/controllers/statistics.js
@@ -3,7 +3,14 @@ function StatisticsController() {
 	this.piechart = {}
 
 	// linechart
-	this.linechart.labels = ["11.08", "12.08", "13.08", "14.08", "15.08", "16.08", "17.08"];
+	this.linechart.labels = [
+		"29.06 - 05.08",
+		"06.07 - 12.07",
+		"13.07 - 19.07",
+		"20.07 - 26.07",
+		"27.07 - 02.08",
+		"03.08 - 09.08",
+		"10.08 - 16.08"];
 	this.linechart.series = ['API Calls', 'File Downloads', 'BibTeX Downloads'];
 	this.linechart.data = [
 		[65, 59, 80, 81, 56, 55, 40],
