67fc41c26285d0cad94ee5ac92d13b60180546c7
Andre Kohn <andre@kohn.io>
2015-08-30 15:38:39 +0200

    Added sample config.json for local development. Added port

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,13 +1,21 @@
 #!/bin/bash
 
-if [ -z "$MC_SERVER" ]; then
-    echo "You need to provide MC_SERVER as environment variable"
+if [ -z "$MC_SERVER_HOSTNAME" ]; then
+    echo "You need to provide MC_SERVER_HOSTNAME as environment variable"
+    exit 1
+fi
+
+if [ -z "$MC_SERVER_PORT" ]; then
+    echo "You need to provide MC_SERVER_PORT as environment variable"
     exit 1
 fi
 
 cat >/usr/share/nginx/html/config.json <<EOL
 var mcConfig = {
-	"server": "$MC_SERVER"
+	"cache": {
+		"hostname": "$MC_SERVER_HOSTNAME",
+		"port": "$MC_SERVER_PORT"
+	}
 }
 EOL
 
