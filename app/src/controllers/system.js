06c6a844d0537d88d8eb8dd15bb18299d836eb10
Andre Kohn <andre@kohn.io>
2015-08-31 09:16:21 +0200

    Using integrating local cache in system controller

--- app/src/controllers/system.js
+++ app/src/controllers/system.js
@@ -1,31 +1,66 @@
-function SystemController($log, $timeout, ServiceConfiguration, SystemApi) {
+function SystemController($log, $timeout, ServiceConfiguration, LocalCache, SystemApi) {
 	this.config = ServiceConfiguration;
+	this.cache = LocalCache
 	this.systemApi = SystemApi;
 	
 	this.$log = $log;
 	this.$timeout = $timeout;
 
-	// Status
+	// Disable the loading circles
 	this.loadingStatus = false;
-	this.clientVersion = this.config.client.version;
-	this.serverVersion = "Unknown";
-	this.mendeleyStatus = "Unknown";
-	this.lastUpdate = "Unknown";
-
-	// Entities
 	this.loadingEntities = false;
-	this.profiles = "Unknown";
-	this.uniqueProfiles = "";
-	this.profileToDocumentLinks = "Unknown";
-	this.documents = "Unknown";
-	this.uniqueDocuments = "";
-	this.documentToFieldLinks = "Unknown";
-	this.researchFields = "Unknown";
 
-	// Reload
-	this.reloadStatus();
-	this.reloadEntities();
+	if(this.cache.hasSystemStatus()) {
+		data = this.cache.getSystemStatus();
+		this.setStatus(data);
+	} else {
+		this.setStatus(null);
+		this.reloadStatus();
+	}
+
+	// Check if items from /system/entities are cached
+	if(this.cache.hasSystemEntities()) {
+		data = this.cache.getSystemEntities();
+		this.setEntities(data);
+	} else {
+		this.setEntities(null);
+		this.reloadEntities();
+	}
 };
+
+SystemController.prototype.setStatus = function(data) {
+	if(data == null) {
+		this.clientVersion 		= 	this.config.client.version;
+		this.serverVersion 		= 	"Unknown";
+		this.mendeleyStatus 	= 	"Unknown";
+		this.lastUpdate 		=	"Unknown";
+	} else {
+		this.clientVersion 		= 	this.config.client.version;
+		this.serverVersion 		= 	data["serverVersion"];
+		this.mendeleyStatus 	= 	data["mendeleyStatus"];
+		this.lastUpdate 		= 	data["lastUpdate"];
+	}
+}
+
+SystemController.prototype.setEntities = function(data) {
+	if(data == null) {
+		this.profiles 				=	"Unknown";
+		this.uniqueProfiles 		= 	"";
+		this.profileToDocumentLinks = 	"Unknown";
+		this.documents 				=	"Unknown";
+		this.uniqueDocuments 		=	"";
+		this.documentToFieldLinks 	= 	"Unknown";
+		this.researchFields 		=	"Unknown";
+	} else {
+		this.profiles 				=	data["profile"];
+		this.uniqueProfiles 		=	"( " + data["cache_profile"] + " unique )";
+		this.profileToDocumentLinks = 	data["cache_profile_has_cache_document"];
+		this.documents 				= 	data["document"];
+		this.uniqueDocuments 		=	"( " + data["cache_document"] + " unique )";
+		this.documentToFieldLinks 	=	data["cache_document_has_cache_field"];
+		this.researchFields 		=	data["cache_field"];
+	}
+}
 
 SystemController.prototype.reloadStatus = function() {
 	this.loadingStatus = true;
@@ -34,9 +69,8 @@ SystemController.prototype.reloadStatus = function() {
 	self = this;
 	promise.then(function(data) {
 		self.$log.info("Successfully fetched system status");
-		self.serverVersion = data.serverVersion;
-		self.mendeleyStatus = data.mendeleyStatus;
-		self.lastUpdate = data.lastUpdate
+		self.cache.setSystemStatus(data);
+		self.setStatus(data);
 	}).catch(function(error) {
 		self.$log.warn("Could not load the system status");
 	}).finally(function() {
@@ -51,14 +85,8 @@ SystemController.prototype.reloadEntities = function() {
 	self = this;
 	promise.then(function(data) {
 		self.$log.info("Successfully fetched system entities");
-		self.profiles = data["profile"];
-		self.uniqueProfiles = "( " + data["cache_profile"] + " unique )";
-		self.profileToDocumentLinks = data["cache_profile_has_cache_document"];
-		self.documents = data["document"];
-		self.uniqueDocuments = "( " + data["cache_document"] + " unique )";
-		self.documentToFieldLinks = data["cache_document_has_cache_field"];
-		self.researchFields = data["cache_field"];
-
+		self.cache.setSystemEntities(data);
+		self.setEntities(data);
 	}).catch(function(error) {
 		self.$log.warn("Could not load the system status");
 	}).finally(function() {
--- app/src/services/local-cache.js
+++ app/src/services/local-cache.js
@@ -2,7 +2,36 @@ function LocalCache() {
 	this.authors = [];
 	this.fields = [];
 
+	this.systemEntities = null;
+	this.systemStatus = null;
+
 }
+
+
+// /system/status
+LocalCache.prototype.hasSystemEntities = function() {
+	return this.systemEntities != null;
+}
+LocalCache.prototype.getSystemEntities = function() {
+	return this.systemEntities
+}
+LocalCache.prototype.setSystemEntities = function(entities) {
+	this.systemEntities = entities;
+}
+
+
+// /system/entities
+LocalCache.prototype.hasSystemStatus = function() {
+	return this.systemStatus != null;
+}
+LocalCache.prototype.getSystemStatus = function() {
+	return this.systemStatus
+}
+LocalCache.prototype.setSystemStatus = function(status) {
+	this.systemStatus = status;
+}
+
+
 
 // Authors
 LocalCache.prototype.getAuthors = function() {
