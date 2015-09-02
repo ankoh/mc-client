94cefd2df18205fb951fd4df37a2e07e027ac42b
Andre Kohn <andre@kohn.io>
2015-09-02 16:25:53 +0200

    Changed the client sided unification method

--- app/src/services/converter.js
+++ app/src/services/converter.js
@@ -5,14 +5,23 @@ function Converter() {
 Converter.prototype.getUnifiedFields = function (tags) {
 	// given comma separated tags,
 	// reproduce the fields as they're created on the server side
-	var splitted = tags.replace(/ /g, '').toLowerCase().split(",");
+	var splitted = tags.replace(/ /g, ' ').toLowerCase().split(",");
 	var trimmed = splitted.filter(function(x) { return x ? true : false; })
 	var unified = trimmed.map(function(tag) { 
+		// first capitalize each word after a dash (cyber-physical -> Cyber-Physical)
 		var words = tag.split('-');
 		var words = words.map(function(word) {
 			 return word.charAt(0).toUpperCase() + word.slice(1)
 		})
-		return words.join(" ")
+		var words = words.join('-');
+
+		// then capitalize each word after a space
+		var words = words.split(' ');
+		var words = words.map(function(word) {
+			 return word.charAt(0).toUpperCase() + word.slice(1)
+		})
+		var words = words.join(' ');
+		return words;
 	});
 
 	return unified;
